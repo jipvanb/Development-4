c153e669b0e184539d4ac699ea34579021939ab7
Dominique <s1169919@student.windesheim.nl>
2022-05-09 14:01:27 +0200

    idk

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jipva\Desktop\school\development 4\dev4\venv
+set VIRTUAL_ENV=C:\Users\domik\Desktop\school\development 4\dev4\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
